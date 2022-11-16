e09f4531bf7cefad4420c2ae4c940ff669452a9b
Konstantin Terziev <kd.terziev@gmail.com>
2022-11-16 04:07:20 +0000

    add POST onfido SDK token (wrong route)

--- src/onfido/onfido.router.js
+++ src/onfido/onfido.router.js
@@ -25,7 +25,7 @@ onfidoRouter.get("/one", checkJwt, async (req, res) => {
 
 // POST onfido/
 
-onfidoRouter.get("/sdk-token", checkJwt, async (req, res) => {
+onfidoRouter.post("/sdk-token", checkJwt, async (req, res) => {
   const applicantId = await ensureApplicantId();
   const token = await generateSdkToken(applicantId, req.body["application_id"]);
   res.status(200).send(token);
