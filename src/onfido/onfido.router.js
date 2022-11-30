4d3b022cedbb821dca88b26cec9aa9e04ebe62a0
Konstantin Terziev <kd.terziev@gmail.com>
2022-11-30 08:28:40 +0000

    add onfido documents and checks endpoints

--- src/onfido/onfido.router.js
+++ src/onfido/onfido.router.js
@@ -3,7 +3,14 @@
  */
 
 const express = require("express");
-const { getOne, ensureApplicantId, generateSdkToken } = require("./onfido.service");
+const {
+  getOne,
+  ensureApplicantId,
+  getDocuments,
+  generateSdkToken,
+  getChecks,
+  createCheck,
+} = require("./onfido.service");
 const { checkJwt } = require("../authz/check-jwt");
 
 /**
@@ -23,12 +30,30 @@ onfidoRouter.get("/one", checkJwt, async (req, res) => {
   res.status(200).send(one);
 });
 
+onfidoRouter.get("/documents", checkJwt, async (req, res) => {
+  const applicantId = await ensureApplicantId(req.user);
+  const documents = await getDocuments(applicantId);
+  res.status(200).send(documents);
+});
+
+onfidoRouter.get("/checks", checkJwt, async (req, res) => {
+  const applicantId = await ensureApplicantId(req.user);
+  const checks = await getChecks(applicantId);
+  res.status(200).send(checks);
+});
+
 // POST onfido/
 
 onfidoRouter.post("/sdk-token", checkJwt, async (req, res) => {
   const applicantId = await ensureApplicantId(req.user);
   const token = await generateSdkToken(applicantId, req.body["application_id"]);
   res.status(200).send(token);
+});
+
+onfidoRouter.post("/checks", checkJwt, async (req, res) => {
+  const applicantId = await ensureApplicantId(req.user);
+  const check = await createCheck(applicantId, req.body["report_names"]);
+  res.status(200).send(check);
 });
 
 module.exports = {
--- src/onfido/onfido.service.js
+++ src/onfido/onfido.service.js
@@ -6,11 +6,16 @@ const { db } = require("../db/postgre");
 const { onfido } = require("./onfido.client");
 
 const selectApplicantIdFromOnfidoByUserId = (user) => {
-  return db.oneOrNone('SELECT applicant_id FROM "onfido" WHERE user_id = $1', [user.sub]);
+  return db.oneOrNone('SELECT applicant_id FROM "onfido" WHERE user_id = $1', [
+    user.sub,
+  ]);
 };
 
 const insertOnfido = async (user, applicant) => {
-  await db.none('INSERT INTO onfido(user_id, applicant_id) VALUES($1, $2)', [user.sub, applicant.id]);
+  await db.none("INSERT INTO onfido(user_id, applicant_id) VALUES($1, $2)", [
+    user.sub,
+    applicant.id,
+  ]);
 };
 
 const getOne = () => {
@@ -18,29 +23,48 @@ const getOne = () => {
 };
 
 const ensureApplicantId = async (user) => {
-  return selectApplicantIdFromOnfidoByUserId(user)
-    .then((row) => {
-      return (row !== null)
-        ? row["applicant_id"]
-        : onfido.applicant.create({
-          firstName: user["given_name"],
-          lastName: user["family_name"],
-        }).then((applicant) => {
-          insertOnfido(user, applicant);
-          return applicant.id;
-        });
-    });
+  return selectApplicantIdFromOnfidoByUserId(user).then((row) => {
+    return row !== null
+      ? row["applicant_id"]
+      : onfido.applicant
+          .create({
+            firstName: user["given_name"],
+            lastName: user["family_name"],
+          })
+          .then((applicant) => {
+            insertOnfido(user, applicant);
+            return applicant.id;
+          });
+  });
+};
+
+const getDocuments = (applicantId) => {
+  return onfido.document.list(applicantId);
 };
 
 const generateSdkToken = (applicantId, applicationId) => {
   return onfido.sdkToken.generate({
     applicantId,
     applicationId,
-  })
+  });
+};
+
+const getChecks = (applicantId) => {
+  return onfido.check.list(applicantId);
+};
+
+const createCheck = (applicantId, reportNames) => {
+  return onfido.check.create({
+    applicantId,
+    reportNames,
+  });
 };
 
 module.exports = {
   getOne,
   ensureApplicantId,
+  getDocuments,
   generateSdkToken,
+  getChecks,
+  createCheck,
 };
