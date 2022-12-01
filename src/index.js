b0317c30513ed7e2d1dd71ae9d6a35e3b36eb673
Konstantin Terziev <kd.terziev@gmail.com>
2022-12-01 15:38:42 +0000

    BUGFIX errors crashing app

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
  */
 
 const express = require("express");
-const actuator = require('express-actuator');
+const actuator = require("express-actuator");
 const cors = require("cors");
 const helmet = require("helmet");
 const { clientOrigins, serverPort } = require("./config/env.dev");
@@ -11,6 +11,8 @@ const { clientOrigins, serverPort } = require("./config/env.dev");
 const { messagesRouter } = require("./messages/messages.router");
 const { onfidoRouter } = require("./onfido/onfido.router");
 const { closeDb } = require("./db/postgre");
+const { UnauthorizedError } = require("express-jwt");
+const { OnfidoApiError } = require("@onfido/api");
 
 /**
  * App Variables
@@ -33,16 +35,32 @@ app.use("/api", apiRouter);
 apiRouter.use("/messages", messagesRouter);
 apiRouter.use("/onfido", onfidoRouter);
 
-app.use(function (err, req, res, next) {
-  console.log(err);
-  res.status(500).send(err.message);
+app.use((err, req, res, next) => {
+  switch (err.constructor) {
+    case UnauthorizedError:
+      console.log(`ExpressJwtError[${err.code}]`);
+      return res.status(err.status).send({
+        error: {
+          type: err.code,
+          message: err.message,
+        },
+      });
+    case OnfidoApiError:
+      console.log(`OnfidoApiError[${err.type}] user_id=${req.user.sub}`);
+      return res.status(err.statusCode).send(err.responseBody);
+    default:
+      console.error(err);
+      return res.status(500).send(err.message);
+  }
 });
 
 /**
  * Server Activation
  */
 
-const server = app.listen(serverPort, () => console.log(`API Server listening on port ${serverPort}`));
+const server = app.listen(serverPort, () =>
+  console.log(`API Server listening on port ${serverPort}`)
+);
 
 process.on("SIGTERM", () => {
   server.close(async () => await closeDb());
--- src/onfido/onfido.router.js
+++ src/onfido/onfido.router.js
@@ -3,6 +3,7 @@
  */
 
 const express = require("express");
+const asyncMiddleware = require("async-middleware");
 const {
   getOne,
   ensureApplicantId,
@@ -18,6 +19,7 @@ const { checkJwt } = require("../authz/check-jwt");
  */
 
 const onfidoRouter = express.Router();
+const wrap = asyncMiddleware.wrap;
 
 /**
  * Controller Definitions
@@ -25,36 +27,59 @@ const onfidoRouter = express.Router();
 
 // GET onfido/
 
-onfidoRouter.get("/one", checkJwt, async (req, res) => {
-  const one = await getOne();
-  res.status(200).send(one);
-});
+onfidoRouter.get(
+  "/one",
+  checkJwt,
+  wrap(async (req, res) => {
+    const one = await getOne();
+    res.status(200).send(one);
+  })
+);
 
-onfidoRouter.get("/documents", checkJwt, async (req, res) => {
-  const applicantId = await ensureApplicantId(req.user);
-  const documents = await getDocuments(applicantId);
-  res.status(200).send(documents);
-});
+onfidoRouter.get(
+  "/documents",
+  checkJwt,
+  wrap(async (req, res) => {
+    const applicantId = await ensureApplicantId(req.user);
+    const documents = await getDocuments(applicantId);
+    res.status(200).send(documents);
+  })
+);
 
-onfidoRouter.get("/checks", checkJwt, async (req, res) => {
-  const applicantId = await ensureApplicantId(req.user);
-  const checks = await getChecks(applicantId);
-  res.status(200).send(checks);
-});
+onfidoRouter.get(
+  "/checks",
+  checkJwt,
+  wrap(async (req, res) => {
+    const applicantId = await ensureApplicantId(req.user);
+    const checks = await getChecks(applicantId);
+    res.status(200).send(checks);
+  })
+);
 
 // POST onfido/
 
-onfidoRouter.post("/sdk-token", checkJwt, async (req, res) => {
-  const applicantId = await ensureApplicantId(req.user);
-  const token = await generateSdkToken(applicantId, req.body["application_id"]);
-  res.status(200).send({ sdk_token: token });
-});
+onfidoRouter.post(
+  "/sdk-token",
+  checkJwt,
+  wrap(async (req, res) => {
+    const applicantId = await ensureApplicantId(req.user);
+    const token = await generateSdkToken(
+      applicantId,
+      req.body["application_id"]
+    );
+    res.status(200).send({ sdk_token: token });
+  })
+);
 
-onfidoRouter.post("/checks", checkJwt, async (req, res) => {
-  const applicantId = await ensureApplicantId(req.user);
-  const check = await createCheck(applicantId, req.body["report_names"]);
-  res.status(200).send(check);
-});
+onfidoRouter.post(
+  "/checks",
+  checkJwt,
+  wrap(async (req, res) => {
+    const applicantId = await ensureApplicantId(req.user);
+    const check = await createCheck(applicantId, req.body["report_names"]);
+    res.status(200).send(check);
+  })
+);
 
 module.exports = {
   onfidoRouter,
